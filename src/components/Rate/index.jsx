6f2d7bd0d5e2a9dc936d700be0d4b045cb2f627d
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-13 22:31:19 +0200

    vyřešeno - přidán useEffect s API

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
 const currencies = {
@@ -8,11 +9,23 @@ const currencies = {
 
 export const Rate = ({from}) => {
   // console.log(currencies[from].CZK)
+  const [currency, setCurrency] = useState("USD")
+
+  useEffect (() => {
+    const fetchCurrency = async() => {
+      const response = await fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+      const json = await response.json()
+      setCurrency(json.rates.CZK)
+    }
+
+    fetchCurrency()
+  }, [from])
+
   return (
     <div className="rate">
       <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value"> {currencies[from].CZK} CZK</div>
+      <div className="rate__value"> {currency} CZK</div>
     </div>
   );
 };
